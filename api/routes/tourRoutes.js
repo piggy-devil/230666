c8b11dec1bbf972f5788a449dc3c54f53f6d2b73
Invisible <invisible@Invisibles-MacBook-Air.local>
2023-06-29 16:13:59 +0700

    initial commit

---
+++ api/routes/tourRoutes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
+// const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+
+// POST  /tour/636cfe26bbd141a256dd6c28/reviews
+// GET  /tour/636cfe26bbd141a256dd6c28/reviews
+// GET  /tour/636cfe26bbd141a256dd6c28/reviews/5c88fa8cf4afda39709c2970
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
+// ใช้ const router = express.Router({ mergeParams: true }); แทนใน reviewRoutes.js (reviewRouter)
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
+
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/-40,45/unit/mi
+
+router
+  .route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
+
+module.exports = router;
